bbfcd8974ce8bf2244972aa412bcccd762342998
FShinoda <fernanda.shinoda@hotmail.com>
2024-04-23 10:47:54 -0300

    feat: buton event listener add
    - recalcutate childDiv according to grid proportion

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // GLOBALS
 const containerDiv = document.querySelector("#container");
+const newGridBtn = document.querySelector("#newGridBtn");
 
 // FUNCTIONS
-function createGrid(){
+function createGrid(proportion=8){
+    console.log(containerDiv.offsetWidth);
+    containerDiv.innerHTML = '';
+    sizeOfSquare = containerDiv.offsetWidth / proportion -2; // -2 is due to border 1px each side!
+    proportion *= proportion;
 
-    for (let i = 0; i < 16; i++){
+    console.log(sizeOfSquare)
+
+    for (let i = 0; i < proportion; i++){
         childDiv = document.createElement("div");
+        childDiv.style.width = `${sizeOfSquare}px`;
+        childDiv.style.height = `${sizeOfSquare}px`;
         childDiv.classList.add("childDiv")
         containerDiv.appendChild(childDiv);
     };
 
 }
-
 
 // MAIN
 createGrid();
@@ -19,7 +27,15 @@ createGrid();
 // LISTENERS
 // Change grid color on mouse over
 containerDiv.addEventListener('mouseover', (event)=>{
-    console.log(event.target)
     event.target.classList.remove("childDiv");
     event.target.classList.add('childDivOnHover')
+});
+
+// New grid button listener
+newGridBtn.addEventListener("click", (event)=>{
+    let proportion;
+    do{
+        proportion = prompt('How many squares the new grid will have each side?');
+    } while (proportion <= 0 || proportion > 50);
+    createGrid(proportion);
 })
